522807494b733fddb705327ee3a74415f53c1d0f
som <chandelmeenaxi@gmail.com>
2018-04-02 11:43:50 +0530

    starting with rails vue app

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="row">
+    <div v-for="list in original_lists" class="col-md-3">
+      <h3>{{ list.name }}</h3>
+      <hr/>
+
+      <div v-for="(card, index) in list.cards" class="card card-body">
+        <h3>{{ card.name }}</h3>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["original_lists"],
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
