9b6fd7a63f61c6a4a6ccd64b84fea6a4f5ce0876
som <chandelmeenaxi@gmail.com>
2018-04-04 10:32:00 +0530

    move cards from on tray to another and move position of card in one tray using drag and drop

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,9 +4,11 @@
       <h3>{{ list.name }}</h3>
       <hr/>
 
-      <div v-for="(card, index) in list.cards" class="card card-body mb-3">
-        <h3>{{ card.name }}</h3>
-      </div>
+      <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
+        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+          <h3>{{ card.name }}</h3>
+        </div>
+      </draggable>
 
       <div class="card card-body">
         <textarea v-model="messages[list.id]" class="form-control"></textarea>
@@ -33,6 +35,32 @@ export default {
   },
 
   methods: {
+    cardMoved: function(event) {
+      const evt = event.added || event.moved
+      if (evt == undefined) { return }
+
+      const element = evt.element
+
+      const list_index = this.lists.findIndex((list) => {
+        return list.cards.find((card) => {
+          return card.id === element.id
+        })
+      })
+
+      var card_data = new FormData
+      card_data.append("card[list_id]", this.lists[list_index].id)
+      card_data.append("card[position]", evt.newIndex + 1)
+
+      Rails.ajax({
+        url: `/cards/${element.id}/move`,
+        type: "PATCH",
+        data: card_data,
+        dataType: "json",
+        beforeSend: () => {
+          return true
+        },
+      })
+    },
     listMoved: function(event) {
       var list_data = new FormData;
       list_data.append("list[position]", event.newIndex + 1)
